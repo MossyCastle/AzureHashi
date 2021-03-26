fec30aedaa93e084695b8e29adb4a1337f9da313
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-03-26 09:19:35 -0400

    Update tfvars.tf

--- tfvars.tf
+++ tfvars.tf
@@ -3,11 +3,11 @@ public_key_file = "/path/to/public-key"
 private_key_file = "/path/to/private-key"
 
 # Azure Provider Variables
-azure_subscription_id = "YOUR_AZURE_SUBSCRIPTION_ID"
-azure_client_id = "YOUR_AZURE_CLIENT_ID"
+azure_subscription_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
+azure_client_id = "c30633ef-4591-4126-8676-4b4468cc3444"
 azure_client_secret = "YOUR_AZURE_CLIENT_SECRET"
-azure_tenant_id = "YOUR_AZURE_TENANT_ID"
-azure_location = "YOUR_AZURE_LOCATION"
+azure_tenant_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
+azure_location = "useast"
 
 # AWS Instance Variables
 azure_servers = 1
