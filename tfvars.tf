154d8ea40591a067c27fb3eecbcafef386a2c77f
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-22 14:54:21 -0400

    Update tfvars.tf

--- tfvars.tf
+++ tfvars.tf
@@ -4,8 +4,8 @@ private_key_file = "/path/to/private-key"
 
 # Azure Provider Variables
 azure_subscription_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
-azure_client_id = "c30633ef-4591-4126-8676-4b4468cc3444"
-azure_client_secret = "YOUR_AZURE_CLIENT_SECRET"
+azure_client_id = "a82df2b4-c77e-4fd6-9d4c-4b9b3d73ac23"
+azure_client_secret = "-x1SqBQN7mb5o_RG3CR2S~.a_inbjmAQiz"
 azure_tenant_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
 azure_location = "useast"
 
