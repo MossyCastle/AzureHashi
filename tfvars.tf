4633a0ef2b311abfe81d050b40b221c87c35f837
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-22 15:09:10 -0400

    Update tfvars.tf

--- tfvars.tf
+++ tfvars.tf
@@ -1,13 +1,33 @@
-# Global Variables 
-public_key_file = "/path/to/public-key"
-private_key_file = "/path/to/private-key"
+//# Global Variables 
+// public_key_file = "/path/to/public-key"
+// private_key_file = "/path/to/private-key"
 
+?*
 # Azure Provider Variables
 azure_subscription_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
 azure_client_id = "a82df2b4-c77e-4fd6-9d4c-4b9b3d73ac23"
 azure_client_secret = "-x1SqBQN7mb5o_RG3CR2S~.a_inbjmAQiz"
 azure_tenant_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
 azure_location = "useast"
+*/
 
+variable subscriptionID {
+  type = string
+  }
+
+variable clientID {
+  type = string
+  }
+
+variable clientSecret {
+  type = string
+  }
+
+variable tennantID {
+  type = string
+  }
+
+/*
 # AWS Instance Variables
 azure_servers = 1
+*/
