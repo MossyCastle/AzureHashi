1a9035f8658a6985f03b63bb178a8f295c6d5c2d
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-23 08:29:02 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 // public_key_file = "/path/to/public-key"
 // private_key_file = "/path/to/private-key"
 
-?*
+/*
 # Azure Provider Variables
 azure_subscription_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
 azure_client_id = "a82df2b4-c77e-4fd6-9d4c-4b9b3d73ac23"
