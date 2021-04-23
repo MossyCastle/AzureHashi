99212d35b3d0f21e42d7e2b47aabb5dd18988ee0
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-23 10:26:05 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,9 +8,10 @@ azure_subscription_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
 azure_client_id = "a82df2b4-c77e-4fd6-9d4c-4b9b3d73ac23"
 azure_client_secret = "-x1SqBQN7mb5o_RG3CR2S~.a_inbjmAQiz"
 azure_tenant_id = "2f1d9ed6-83cf-492d-993b-ac76aa12894c"
-azure_location = "useast"
+azure_location = "eastus"
 */
 
+/*
 variable subscriptionID {
   type = string
   }
@@ -27,7 +28,6 @@ variable tennantID {
   type = string
   }
 
-/*
 # AWS Instance Variables
 azure_servers = 1
 */
