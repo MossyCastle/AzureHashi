3cf23ec8ed7aa254f68c08331dd4cc275dfe9c4d
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-05-19 14:48:31 -0400

    Create dp900.tf

---
+++ dp900.tf
@@ -0,0 +1,56 @@
+/*
+  create environment for dp900 exam prep
+
+*/
+// create learning resource goup
+resource "azurerm_resource_group" "DP900_moss" {
+  name     = "mossy-example-resources"
+  location = "eastus"
+}    
+    
+//  create storage account
+
+resource "azurerm_storage_account" "DP900_moss" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.DP900_moss.name
+  location                 = azurerm_resource_group.DP900_moss.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "DP900_moss" {
+  name               = "example"
+  storage_account_id = azurerm_storage_account.example.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
+}    
+    
+// create synapse workspace
+resource "azurerm_synapse_workspace" "DP900-mosstraining" {
+  name                                 = "Moss-training"
+  resource_group_name                  = azurerm_resource_group.DP900_moss.name
+  location                             = azurerm_resource_group.DP900_moss.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.DP900_moss.id
+  sql_administrator_login              = "sqladminuser"
+  sql_administrator_login_password     = "H@Sh1CoR3!"
+
+  aad_admin {
+    login     = "AzureAD Admin"
+    object_id = "00000000-0000-0000-0000-000000000000"
+    tenant_id = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    Env = "learning"
+  }
+}    
+    
+//  ejdcccrccihkrvtgcffnfhtdtglfljbbictdcecgnecn
+    
+    
+ // create power BI capability
+    
